05a619904bf7b8231591cf5a6016b36049df1407
OrShahar1 <63114851+OrShahar1@users.noreply.github.com>
2020-05-26 10:10:57 +0300

    version 3
    version 3

---
+++ todo/Cake_bot_Gui.py
@@ -0,0 +1,122 @@
+import sys
+import json
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtCore import Qt
+from send_mail import caller
+from multiprocessing import Process
+from database_mng import sort_database
+import speech_recognition as sr
+from gtts import gTTS
+import os
+import time
+import playsound
+
+qt_creator_file = "mainwindow.ui"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qt_creator_file)
+tick = QtGui.QImage('tick.png')
+
+
+def speak(text):
+    tts = gTTS(text=text, lang='en')
+    filename = 'voice'+text+'.mp3'
+    tts.save(filename)
+    playsound.playsound(filename)
+
+class TodoModel(QtCore.QAbstractListModel):
+    def __init__(self, *args, todos=None, **kwargs):
+        super(TodoModel, self).__init__(*args, **kwargs)
+        self.members_list = todos or []
+        
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            _, text = self.members_list[index.row()]
+            return text
+        
+        if role == Qt.DecorationRole:
+            status, _ = self.members_list[index.row()]
+            if status:
+                return tick
+
+    def rowCount(self, index):
+        return len(self.members_list)
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.model = TodoModel()
+        self.load()
+        self.todoView.setModel(self.model)
+        self.addButton.pressed.connect(self.add)
+        self.deleteButton.pressed.connect(self.delete)
+        self.completeButton.pressed.connect(self.complete)
+
+    def add(self ):
+        text = self.todoEdit.text()
+        if text: # Don't add empty strings.
+            # Access the list via the model.
+            self.model.members_list.append((True, text))
+            # Trigger refresh.        
+            self.model.layoutChanged.emit()
+            # Empty the input
+            self.todoEdit.setText("")
+            self.save()
+            speak("adding member")
+        
+    def delete(self):
+        indexes = self.todoView.selectedIndexes()
+        if indexes:
+            # Indexes is a list of a single item in single-select mode.
+            index = indexes[0]
+            # Remove the item and refresh.
+            del self.model.members_list[index.row()]
+            self.model.layoutChanged.emit()
+            # Clear the selection (as it is no longer valid).
+            self.todoView.clearSelection()
+            self.save()
+            speak("Deleting member")
+            
+    def complete(self):
+        indexes = self.todoView.selectedIndexes()
+        if indexes :
+            index = indexes[0]
+            row = index.row()
+            status, text = self.model.members_list[row]
+            self.model.members_list[row] = (True, text)
+            self.model.dataChanged.emit(index, index)
+            self.todoView.clearSelection()
+            self.save()
+            sort_database()
+            speak("mail was sent ")
+            speak("please change database order manually")
+            speak("Move the member you selected to the end of the list")
+            p = Process(target=caller(text))
+            p.start()
+            p.join()
+
+
+    def load(self):
+        try:
+            with open('data.db', 'r') as f:
+                self.model.members_list = json.load(f)
+        except Exception:
+            pass
+
+    def save(self):
+        with open('data.db', 'w') as f:
+            data = json.dump(self.model.members_list, f)
+
+
+speak("Hello sir")
+speak("I'm Or's robot")
+speak("I'll help you manage your groups meeting")
+speak("lets start")
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
+
+
+
---
+++ todo/database_mng.py
@@ -0,0 +1,10 @@
+
+
+# todo : wire this function
+
+def sort_database ():
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print ( "~~[ Cake bot : )]~~")
+    print ("mail was sent ")
+    print ("please change database order manually")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
